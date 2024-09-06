c488b7325f5acbd0f0e184003106fab4cd5d65cd
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-09-06 17:02:56 +0500

    renamed the index.cjs to index.js

--- index.cjs
+++
@@ -1,6 +0,0 @@
-const franc = require('franc');
-const lang = require('langs');
-
-const LangCode = franc('Alle menslike wesens word vry');
-const language = lang.where("3",LangCode);
-console.log(language);
